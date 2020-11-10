1d4003c9e2b909fb184935fd10b69a43af309ec0
David <datogele@gmail.com>
2020-11-11 00:51:03 +0400

    types

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //creating first java string
+        String str = "HelloWorld";
+
+        //creating another java string
+        String str2 = "SeeYouSoonWorld";
+
+        //Displaying all strings
+        System.out.println(str);
+        System.out.println(str2);
+
+        //creating first int
+        int num_1 =24;
+
+        //creating second int
+        int num_2 = 7;
+
+        //creating last int
+        int num_3 = 1991;
+
+        //Get my birthday date
+        System.out.println(num_1);
+        System.out.println(num_2);
+        System.out.println(num_3);
+
+        //creating first floating point number
+        float number1 = 1.58f;
+
+        //creating second floating point number
+        float number2 = 1.58f;
+
+        //return number of PI by gathering float numbers
+        System.out.println(number1 + number2);
+
+        //creating Character
+        char firstLetterOfMyName = 'D';
+
+        //print first letter of my name
+        System.out.println(firstLetterOfMyName);
+
+        //creating first boolean
+        boolean isJoeBidenUsaPresident = true;
+
+        //creating second boolean
+        boolean androidIsBetterThanIos = false;
+
+        //print booleans
+        System.out.println(isJoeBidenUsaPresident);
+        System.out.println(androidIsBetterThanIos);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
